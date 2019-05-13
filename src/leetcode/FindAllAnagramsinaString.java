b6f7097ac6a8f17592fe70469b7391306bccd4b5
Robina <robinali34@gmail.com>
2019-05-12 20:38:20 -0700

    Update FindAllAnagramsinaString.java
    Fix wrong return

--- src/leetcode/FindAllAnagramsinaString.java
+++ src/leetcode/FindAllAnagramsinaString.java
@@ -48,21 +48,21 @@ public class FindAllAnagramsinaString {
 
     public static List<Integer> findAnagrams(String s, String p) {
         List<Integer> res = new ArrayList<>();
-        if (s == null || p == null || s.length() < p.length()) return res;
+        if(s == null || p == null || s.length() < p.length()) return res;
         int[] chars = new int[26];
-        for (char c : p.toCharArray()) {
+        for(char c : p.toCharArray()) {
             chars[c - 'a']++;
         }
         int start = 0, end = 0;
         int count = p.length();
-        while (end < s.length()) {
-            if (end - start == p.length() && chars[s.charAt(start++) - 'a'] >= 0) {
-                count++;
-            }
-            if (--chars[s.charAt(end++) - 'a'] >= 0) {
+        while(end < s.length()) {
+            if(chars[s.charAt(end++) - 'a']-- >= 1) {
                 count--;
             }
-            if (count == 0) res.add(start);
+            if(count == 0) res.add(start);
+            if(end - start == p.length() && chars[s.charAt(start++) - 'a']++ >= 0) {
+                count++;
+            }
         }
         return res;
     }
