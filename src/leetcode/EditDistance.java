0cce18fad23e9f4b66ce92181b632020bf3e9fc0
Robina <robinali34@gmail.com>
2019-05-17 21:52:23 -0700

    Update EditDistance.java

--- src/leetcode/EditDistance.java
+++ src/leetcode/EditDistance.java
@@ -59,7 +59,7 @@ public class EditDistance {
                 if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
                     dp[i][j] = dp[i - 1][j - 1];
                 } else {
-                    dp[i][j] = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j]), dp[i -1][j - 1]);
+                    dp[i][j] = Math.min(Math.min(dp[i][j - 1] + 1, dp[i - 1][j] + 1), dp[i - 1][j - 1] + 1);
                 }
             }
         }
