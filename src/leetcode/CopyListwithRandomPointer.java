f70246964aa5237c551d0cc8603f6dee9a85f707
Robina <robinali34@gmail.com>
2019-06-05 20:45:47 -0700

    Update CopyListwithRandomPointer.java

--- src/leetcode/CopyListwithRandomPointer.java
+++ src/leetcode/CopyListwithRandomPointer.java
@@ -53,38 +53,38 @@ public class CopyListwithRandomPointer {
      * @param head
      * @return
      */
-    public RandomListNode copyRandomList2(RandomListNode head) {
-        RandomListNode cur = head;
-        RandomListNode next;
+    public Node copyRandomList2(Node head) {
+        // Time: O(n) Space: O(1)
+        Node cur = head;
+        
         //next copy
-        while (cur != null) {
-            next = cur.next;
-            RandomListNode copy = new RandomListNode(cur.label);
+        while(cur != null) {
+            Node copy = new Node(cur.val);
+            copy.next = cur.next;
             cur.next = copy;
-            copy.next = next;
-            cur = next;
+            cur = copy.next;
         }
+        
         cur = head;
         //random copy
-        while (cur != null) {
+        while(cur != null) {
             if (cur.random != null) {
                 cur.next.random = cur.random.next;
             }
             cur = cur.next.next;
         }
+        
         cur = head;
-        RandomListNode dummy = new RandomListNode(0);
-        RandomListNode copy;
-        RandomListNode copycur = dummy;
-
+        Node next = head.next;
+        Node rtn = head.next;
         while (cur != null) {
-            next = cur.next.next;
-            copy = cur.next;
-            copycur.next = copy;
-            copycur = copy;
-            cur.next = next;
-            cur = next;
+        cur.next = cur.next.next;
+        if (next.next != null) {
+            next.next = next.next.next;
         }
-        return dummy.next;
+        cur = cur.next;
+        next = next.next;
+        }
+        return rtn;
     }
 }
