28d705e7fb41924c93cb7cbb7867d3cf24dbc8bf
Robina <robinali34@gmail.com>
2019-08-15 19:33:18 -0700

    Update MeetingRoomsII.java

--- src/leetcode/MeetingRoomsII.java
+++ src/leetcode/MeetingRoomsII.java
@@ -30,34 +30,34 @@ public class MeetingRoomsII {
      * @param intervals
      * @return
      */
-    public int minMeetingRooms(Interval[] intervals) {
+    public int minMeetingRooms(int[][] intervals) {
         int[] starts = new int[intervals.length];
         int[] ends = new int[intervals.length];
-        for (int i = 0; i < intervals.length; i++) {
-            starts[i] = intervals[i].start;
-            ends[i] = intervals[i].end;
+        for(int i = 0; i < intervals.length; i++) {
+            starts[i] = intervals[i][0];
+            ends[i] = intervals[i][1];
         }
         Arrays.sort(starts);
         Arrays.sort(ends);
         int res = 0;
         int end = 0;
-        for (int i = 0; i < intervals.length; i++) {
-            if (starts[i] < ends[end]) {
+        for(int i = 0; i < intervals.length; i++) {
+            if(starts[i] < ends[end]) {
                 res++;
             } else end++;
         }
         return res;
     }
 
-
-    public int minMeetingRooms2(Interval[] intervals) {
-        Arrays.sort(intervals, (a, b) -> a.start - b.start);
-        PriorityQueue<Interval> heap = new PriorityQueue<>(intervals.length, (a, b) -> a.end - b.end);
+    public int minMeetingRooms(int[][] intervals) {
+        if(intervals == null || intervals.length == 0) return 0;
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        PriorityQueue<int[]> heap = new PriorityQueue<>(intervals.length, (a, b) -> a[1] - b[1]);
         heap.offer(intervals[0]);
-        for (int i = 1; i < intervals.length; i++) {
-            Interval interval = heap.poll();
-            if (intervals[i].start >= interval.end) {
-                interval.end = intervals[i].end;
+        for(int i = 1; i < intervals.length; i++) {
+            int[] interval = heap.poll();
+            if(intervals[i][0] >= interval[1]) {
+                interval[1] = intervals[i][1];
             } else {
                 heap.offer(intervals[i]);
             }
