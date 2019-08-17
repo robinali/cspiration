b677551e4ef76a217ff0fb19421180a7430ec1ee
Robina <robinali34@gmail.com>
2019-08-17 11:50:57 -0700

    Update FindMedianfromDataStream.java

--- src/leetcode/FindMedianfromDataStream.java
+++ src/leetcode/FindMedianfromDataStream.java
@@ -29,24 +29,25 @@ public class FindMedianfromDataStream {
      time : O(logn)
      space : O(n)
      */
-
     private PriorityQueue<Long> small;
     private PriorityQueue<Long> large;
 
-    public FindMedianfromDataStream() {  // MedianFinder()
-        small = new PriorityQueue<>();
+    /** initialize your data structure here. */
+    public MedianFinder() {
+        small = new PriorityQueue<>(Collections.reverseOrder());
+        //small = new PriorityQueue<>((x, y) -> y - x);
         large = new PriorityQueue<>();
     }
-
+    
     public void addNum(int num) {
         large.add((long)num);
-        small.add(-large.poll());
-        if (large.size() < small.size()) {
-            large.add(-small.poll());
+        small.add(large.poll());
+        if(large.size() < small.size()) {
+            large.add(small.poll());
         }
     }
-
+    
     public double findMedian() {
-        return large.size() > small.size() ? large.peek() : (large.peek() - small.peek()) / 2;
+        return large.size() > small.size() ? large.peek() : (large.peek() + small.peek()) / 2.0;
     }
 }
