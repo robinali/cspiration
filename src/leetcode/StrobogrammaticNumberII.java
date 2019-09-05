d30e4df297cd6af04a3aa731f9b52d3803a8160c
Robina <robinali34@gmail.com>
2019-09-04 20:42:53 -0700

    Update StrobogrammaticNumberII.java

--- src/leetcode/StrobogrammaticNumberII.java
+++ src/leetcode/StrobogrammaticNumberII.java
@@ -59,27 +59,25 @@ public class StrobogrammaticNumberII {
         return dp[n];
     }
     
-    
     public List<String> findStrobogrammatic(int n) {
-        return helper(n, n);
+        return findStrobogrammatic(n, n);
     }
-    public List<String> helper(int n, int m) {
-        if (n == 0) return new ArrayList<>(Arrays.asList(""));
-        if (n == 1) return new ArrayList<>(Arrays.asList("0", "1", "8"));
-
-        List<String> list = helper(n - 2, m);
+    
+    private List<String> findStrobogrammatic(int cur, int max) {
+        if(cur == 0) return new ArrayList<>(Arrays.asList(""));
+        if(cur == 1) return new ArrayList<>(Arrays.asList("0", "1", "8"));
+        
         List<String> res = new ArrayList<>();
-
-        for (int i = 0; i < list.size(); i++) {
-            String s = list.get(i);
-            if (n != m) {
-                res.add("0" + s + "0");
-            }
-            res.add("1" + s + "1");
-            res.add("6" + s + "9");
-            res.add("8" + s + "8");
-            res.add("9" + s + "6");
+        List<String> center = findStrobogrammatic(cur - 2, max);
+        
+        for(int i = 0; i < center.size(); i++) {
+            String temp = center.get(i);
+            if(cur != max) res.add("0" + temp + "0");
+            res.add("1" + temp + "1");
+            res.add("6" + temp + "9");
+            res.add("8" + temp + "8");
+            res.add("9" + temp + "6");
         }
         return res;
-    }
+    }   
 }
