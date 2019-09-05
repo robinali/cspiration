4ad7f60233f423a32578bcfe206daf6bd0320ddd
Robina <robinali34@gmail.com>
2019-09-04 19:27:12 -0700

    Update IntegertoEnglishWords.java

--- src/leetcode/IntegertoEnglishWords.java
+++ src/leetcode/IntegertoEnglishWords.java
@@ -22,31 +22,32 @@ public class IntegertoEnglishWords {
 
 
      */
-    String[] less20={"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
-    String[] tens={"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
-    String[] thousands={"", "Thousand", "Million", "Billion"};
-
+    String[] less20 = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    String[] tens = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    String[] thousands = {"", "Thousand", "Million", "Billion"};
+    
     public String numberToWords(int num) {
-        if (num == 0) return "Zero";
-        String res = "";
+        if(num == 0) return "Zero";
+        StringBuilder sb = new StringBuilder("");
         int i = 0;
-        while (num > 0) {
-            if (num % 1000 != 0) {
-                res = helper(num % 1000) + thousands[i] + " " + res;
+        while(num > 0) {
+            if(num % 1000 != 0) {
+                sb.insert(0, (numberToWordsLessThanThousand(num % 1000) + thousands[i] + " "));
             }
             num /= 1000;
             i++;
         }
-        return res.trim();
+        return sb.toString().trim();
     }
-    public String helper(int num) {
-        if (num == 0) return "";
-        if (num < 20) {
+    
+    private String numberToWordsLessThanThousand(int num) {
+        if(num == 0) return "";
+        if(num < 20) {
             return less20[num % 20] + " ";
-        } else if (num < 100) {
-            return tens[num / 10] + " " + helper(num % 10);
+        } else if(num < 100) {
+            return tens[num /10] + " " + numberToWordsLessThanThousand(num % 10);
         } else {
-            return less20[num / 100] + " Hundred " + helper(num % 100);
+            return less20[num / 100] + " Hundred " + numberToWordsLessThanThousand(num % 100);
         }
     }
 }
