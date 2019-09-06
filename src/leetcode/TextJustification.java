06a30fb6be4e4d199d26316cd37cb40c151a18fc
Robina <robinali34@gmail.com>
2019-09-05 19:14:45 -0700

    Update TextJustification.java

--- src/leetcode/TextJustification.java
+++ src/leetcode/TextJustification.java
@@ -43,36 +43,36 @@ public class TextJustification {
      * @param L
      * @return
      */
-    public List<String> fullJustify(String[] words, int L) {
+    public List<String> fullJustify(String[] words, int maxWidth) {
         List<String> res = new ArrayList<>();
         int index = 0;
-        while (index < words.length) {
+        while(index < words.length) {
             int count = words[index].length();
             int last = index + 1;
-            while (last < words.length) {
-                if (words[last].length() + count + 1 > L) break;
+            while(last < words.length) {
+                if(words[last].length() + count + 1 > maxWidth) break;
                 count += 1 + words[last].length();
                 last++;
             }
             StringBuilder builder = new StringBuilder();
             builder.append(words[index]);
             int diff = last - index - 1;
-            if (last == words.length || diff == 0) {
-                for (int i = index + 1; i < last; i++) {
+            if(last == words.length || diff == 0) {
+                for(int i = index + 1; i < last; i++) {
                     builder.append(" ");
                     builder.append(words[i]);
                 }
-                for (int i = builder.length(); i < L; i++) {
+                for(int i = builder.length(); i < maxWidth; i++) {
                     builder.append(" ");
                 }
             } else {
-                int spaces = (L - count) / diff;
-                int r = (L - count) % diff;
-                for (int i = index + 1; i < last; i++) {
-                    for (int k = spaces; k > 0; k--) {
+                int spaces = (maxWidth - count) / diff;
+                int r = (maxWidth - count) % diff;
+                for(int i = index + 1; i < last; i++) {
+                    for(int k = spaces; k > 0; k--) {
                         builder.append(" ");
                     }
-                    if (r > 0) {
+                    if(r > 0) {
                         builder.append(" ");
                         r--;
                     }
