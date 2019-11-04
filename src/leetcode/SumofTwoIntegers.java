ea76e4c53ce279387aa32d3c1452980ec6a46f63
Robina <robinali34@gmail.com>
2019-11-03 18:35:30 -0800

    Update SumofTwoIntegers.java

--- src/leetcode/SumofTwoIntegers.java
+++ src/leetcode/SumofTwoIntegers.java
@@ -23,15 +23,17 @@ public class SumofTwoIntegers {
      * @return
      */
     public int getSum(int a, int b) {
-        if (a == 0) return b;
-        if (b == 0) return a;
-
-        while (b != 0) {
-            int carry = a & b;
-            a = a ^ b;
-            b = carry << 1;
+        if(a == 0) return b;
+        if(b == 0) return a;
+        
+        int sum = a ^ b;
+        int car = (a & b) << 1;
+        
+        while(car != 0) {
+            int curcar = sum & car;
+            sum = sum ^ car;
+            car = curcar << 1;
         }
-
-        return a;
+        return sum;
     }
 }
