e6befe0e00e987edf87b2376b0cf945696e03946
Robina <robinali34@gmail.com>
2019-11-30 19:42:16 -0800

    Update ValidParentheses.java

--- src/leetcode/ValidParentheses.java
+++ src/leetcode/ValidParentheses.java
@@ -45,4 +45,22 @@ public class ValidParentheses {
         }
         return stack.isEmpty();
     }
+    
+    public boolean isValid1(String s) {
+        if (s == null || s.length() == 0) return true;
+        Deque<Character> stack = new ArrayDeque<>();
+        Map<Character, Character> hm = new HashMap<Character, Character>() {{
+            put(')', '(');
+            put('}', '{');
+            put(']', '[');
+        }};
+    
+        for(Character ch : s.toCharArray()) {
+            if(hm.get(ch) == null)
+                stack.push(ch);
+            else if(stack.isEmpty() || hm.get(ch) != stack.pop())
+                return false;
+        }
+        return stack.isEmpty();
+    }
 }
